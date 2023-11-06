5d54eca319c5797370c27208ce96c0115dc3185a
4072-jenish <hariyanijenish@gmail.com>
2023-11-06 19:03:00 +0530

    second

---
+++ Q1.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+   clrscr();
+   printf("My name is Jenish\n");
+   printf("I am 17 yaer old\n");
+   printf("My last school is goverment school of kamrej\n");
+   printf("My couers is flutter\n");
+   printf("My GRID 4072\n");
+   printf("207,Shree vinayak sosayti Mama dev chock kamrej\n");
+    getch();
+}
